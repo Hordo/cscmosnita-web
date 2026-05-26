81956af751f7671c279a84f776f19a74a6326668
Bogdan Hordo <you@yourdomain.com>
2026-05-26 07:43:43 +0300

    fix article news

--- frontend/src/pages/NewsDetailPage.tsx
+++ frontend/src/pages/NewsDetailPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "../styles/adminStyles.css";
 import { useParams, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import api from "../config/axios";
